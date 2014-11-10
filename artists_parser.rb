cab43f60230a9fc775478b538366aa6e615d0a60
Alexandre Quintino <amsquintino@gmail.com>
2014-11-10 11:42:59 +0100

    Finished first version of ArtistsParser

--- artists_parser.rb
+++ artists_parser.rb
@@ -1,26 +1,37 @@
 require_relative 'xml_parser'
 require 'pry'
+require 'csv'
 
 FILE = "data/discogs_20141001_artists.xml"
 KNOWN_TAGS = %w(id name namevariations members aliases images #text realname data_quality profile urls groups)
+CSV_FILE = "artists.csv"
 
-Xml::Parser.new(FILE) do
-  for_element 'artist' do
-    current = {}
-    inside_element do
-      for_element 'id' do current[:id] = inner_xml end
-      for_element 'name' do current[:name] = inner_xml end
-      inside_element 'namevariations' do end
-      inside_element 'members' do end
-      inside_element 'aliases' do end
-      inside_element 'images' do end
-      inside_element 'realname' do end
-      inside_element 'data_quality' do end
-      inside_element 'profile' do end
-      inside_element 'urls' do end
-      inside_element 'groups' do end
-      raise "Don't know what to do with node=#{name} current=#{current}" unless KNOWN_TAGS.include?(name)
+def parse(&block)
+  Xml::Parser.new(FILE) do
+    for_element 'artist' do
+      current = {}
+      inside_element do
+        for_element 'id' do current[:id] = inner_xml end
+        for_element 'name' do current[:name] = inner_xml end
+        inside_element 'namevariations' do end
+        inside_element 'members' do end
+        inside_element 'aliases' do end
+        inside_element 'images' do end
+        inside_element 'realname' do end
+        inside_element 'data_quality' do end
+        inside_element 'profile' do end
+        inside_element 'urls' do end
+        inside_element 'groups' do end
+        raise "Don't know what to do with node=#{name} current=#{current}" unless KNOWN_TAGS.include?(name)
+      end
+      yield current
     end
-    puts "#{current[:name]} - #{current[:id]}"
+  end
+end
+
+headers = %w(discogs_id name)
+CSV.open(CSV_FILE, 'wb', {headers: headers, col_sep: "\t", write_headers: true, encoding: "UTF-8"}) do |csv|
+  parse do |artist|
+    csv << [artist[:id], artist[:name]]
   end
 end
