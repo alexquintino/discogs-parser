b5527f3fddefbf15b55925694ebd14a50c7fcc8a
Alexandre Quintino <amsquintino@gmail.com>
2014-12-15 14:55:59 +0100

    Rename output file to discogs_artists

--- output_artists_csv.rb
+++ output_artists_csv.rb
@@ -3,12 +3,11 @@ require 'pry'
 require 'csv'
 
 FILE = "data/discogs_20141001_artists.xml"
-CSV_FILE = "output/artists.csv"
+CSV_FILE = "output/discogs_artists.csv"
 CSV_HEADERS = %w(discogs_id name)
 
 CSV.open(CSV_FILE, 'wb', {headers: CSV_HEADERS, col_sep: "\t", write_headers: true, encoding: "UTF-8"}) do |csv|
   ArtistsParser.parse(FILE) do |artist|
-    binding.pry
     csv << [artist[:id], artist[:name]]
   end
 end
