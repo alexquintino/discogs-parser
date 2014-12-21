d9d6b8f0020b0a962242647b0ce6720632acc10b
Alexandre Quintino <amsquintino@gmail.com>
2014-12-21 21:13:10 +0100

    Major improvement by using a Sax Parser. Finally works with JRuby without using all the memory

--- artists_parser.rb
+++ artists_parser.rb
@@ -9,55 +9,86 @@ class ArtistsParser < Xml::Parser
 
   def initialize
     @time_start = Time.now
-    @parsed = 0
+  end
+
+  def parse(file, &block)
+    begin
+      document = ArtistDocument.new(block)
+      parser = Nokogiri::XML::SAX::Parser.new(document)
+      parser.parse_file(file)
+    rescue Interrupt
+      time_end = Time.now
+      diff_time = time_end - @time_start
+      puts "Speed=#{document.how_many_parsed / diff_time.to_f}"
+    end
+  end
+end
+
+class ArtistDocument < Nokogiri::XML::SAX::Document
+
+  def initialize(block = nil)
+    @block = block
+    @state = []
+    @parsed_count = 0
     @artist_id, @artist_name= nil
     @parsed_list = ""
     @parsed_list_size = 0
   end
 
-  def parse(file, &block)
-    begin
-      @node = Nokogiri::XML::Reader.from_io(File.new(file))
-      @node.each do
-        if is_start? && @node.name == "artist"
-          handle_artist_node
-          parsed(block)
-        end
-      end
-      block.call @parsed_list
-    rescue Interrupt
-      time_end = Time.now
-      diff_time = time_end - @time_start
-      puts "Speed=#{@parsed / diff_time.to_f}"
+  def start_element(name, attrs = [])
+    case name
+    when "artist"
+      @state.push(:artist)
+    when "name"
+      @state.push(:name) if @state.last == :artist
+    when "id"
+      @state.push(:id)
+    else
+      @state.push(:unknown) if @state.last == :artist
     end
   end
 
-  def handle_artist_node
-    @node.each do
-      if is_start? && @node.name == "name"
-        @artist_name = fix_name(@node.inner_xml)
-      elsif is_start? && @node.name == "id"
-        @artist_id = @node.inner_xml
-      elsif is_end? && @node.name == "artist"
-        break
-      else
-        ignore_node
-      end
+  def end_element(name, attrs = [])
+    case name
+    when "artist"
+      parsed(@block, @artist_id, @artist_name)
+      @artist_id, @artist_name = nil
+      @state.clear
+    when "name"
+      @state.pop if @state.last == :name
+    when "id"
+      @state.pop if @state.last == :id
+    else
+      @state.pop if @state.last == :unknown
     end
   end
 
-  def parsed(block)
-    @parsed_list << "#{@artist_id}\t#{@artist_name}\n"
-    @artist_id, @artist_name = nil
-    @parsed += 1
+  def characters(string)
+    if @state.last == :id
+      @artist_id = string
+    elsif @state.last == :name
+      @artist_name = fix_name(string)
+    end
+  end
+
+  def end_document
+    flush
+  end
+
+  def flush
+    @block.call @parsed_list
+    @parsed_list = ""
+    @parsed_list_size = 0
+  end
+
+  def parsed(block, artist_id, artist_name)
+    @parsed_list << "#{artist_id}\t#{artist_name}\n"
+    @parsed_count += 1
     @parsed_list_size += 1
     if @parsed_list_size == 500000
-      block.call @parsed_list
-      @parsed_list = ""
-      @parsed_list_size = 0
+      flush
     end
   end
-
 
   def fix_name(name)
     number = remove_number!(name)
@@ -71,6 +102,10 @@ class ArtistsParser < Xml::Parser
     number = name.scan(/\(\d+\)/).last
     name.gsub!(/\(\d+\)/, "") if number #remove number and add it to the end later
     number
+  end
+
+  def how_many_parsed
+    @parsed_count
   end
 end
 
--- test/artists_parser_test.rb
+++ test/artists_parser_test.rb
@@ -1,28 +1,28 @@
 require_relative "../artists_parser"
 require "minitest/autorun"
+require "pry"
 
 class ArtistsParserTest < MiniTest::Unit::TestCase
 
   def test_parse
     file = "test/test_xml.xml"
-    result_id, result_name = nil
-    ArtistsParser.parse(file) do |id, artist|
-      result_id = id
-      result_name = artist
+    result = []
+    ArtistsParser.parse(file) do |artist_string|
+      result = artist_string.chomp.split("\t")
     end
-    assert_equal "3", result_id
-    assert_equal "Josh Wink", result_name
+    assert_equal "3", result[0]
+    assert_equal "Josh Wink", result[1]
   end
 
   def test_returns_the_name_correctly_if_theres_nothing_to_be_done
-    assert_equal "Bla", ArtistsParser.new.fix_name("Bla")
+    assert_equal "Bla", ArtistDocument.new.fix_name("Bla")
   end
 
   def test_reverses_the_name_correctly
-    assert_equal "Cenas Bla", ArtistsParser.new.fix_name("Bla, Cenas")
+    assert_equal "Cenas Bla", ArtistDocument.new.fix_name("Bla, Cenas")
   end
 
   def test_moves_the_number_correctly
-    assert_equal "Cenas Bla (6)", ArtistsParser.new.fix_name("Bla, Cenas (6)")
+    assert_equal "Cenas Bla (6)", ArtistDocument.new.fix_name("Bla, Cenas (6)")
   end
 end
