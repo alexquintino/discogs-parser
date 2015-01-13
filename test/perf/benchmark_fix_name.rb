d288b9e0727bc19f6b9fdaec328ade36c831c8ac
Alexandre Quintino <amsquintino@gmail.com>
2015-01-13 13:57:17 +0100

    Add celluloid to gemfile

--- test/perf/benchmark_fix_name.rb
+++ test/perf/benchmark_fix_name.rb
@@ -1,5 +1,5 @@
 require "benchmark/ips"
-require_relative "../artists_parser"
+require_relative "../../lib/artists_document"
 require "celluloid"
 
 Benchmark.ips do |x|
@@ -7,7 +7,7 @@ Benchmark.ips do |x|
   x.warmup = 2
   x.time = 20
 
-  document = ArtistDocument.new
+  document = ArtistsDocument.new
   text = "Persuader, The (6)"
   x.report("simple") { document.fix_name(text) }
   x.report("celluloid") { Celluloid::Future.new { document.fix_name(text) }}
