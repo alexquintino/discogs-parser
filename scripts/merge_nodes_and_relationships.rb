04d746e44289cf5b40a0a6d1c4ed05f1ce41350c
Alexandre Quintino <amsquintino@gmail.com>
2015-01-28 15:44:34 +0100

    Add label type to tracklist and track nodes. Add script to merge all nodes and relationships into one file

---
+++ scripts/merge_nodes_and_relationships.rb
@@ -0,0 +1,43 @@
+require "tempfile"
+
+HEADERS_ARTISTS = %w(discogs_id:int name:string l:label)
+HEADERS_TRACKLISTS = %w(discogs_id:int title:string l:label)
+HEADERS_TRACKS = %w(discogs_id:int title:string l:label)
+HEADERS_RELATIONSHIPS = %w(start end type)
+
+def output_artists_tsv
+  headers = Tempfile.new("artists_headers")
+  headers.write HEADERS_ARTISTS.join("\t") + "\n"
+  headers.close
+  %x( cat #{headers.path} output/artists_nodes/part-* > output/artists_nodes.tsv )
+  headers.unlink
+end
+
+def output_tracklists_tsv
+  headers = Tempfile.new("tracklists_headers")
+  headers.write HEADERS_TRACKLISTS.join("\t") + "\n"
+  headrs.close
+  %x( cat #{headers.path} output/tracklists_nodes/part-* > output/tracklists_nodes.tsv )
+  headers.unlink
+end
+
+def output_tracks_tsv
+  headers = Tempfile.new("tracks_headers")
+  headers.write HEADERS_TRACKS.join("\t") + "\n"
+  headrs.close
+  %x( cat #{headers.path} output/tracks_nodes/part-* > output/tracks_nodes.tsv )
+  headers.unlink
+end
+
+def output_relationships_tsv
+  headers = Tempfile.new("relationships_headers")
+  headers.write HEADERS_RELATIONSHIPS + "\n"
+  headers.close
+  %x( cat #{headers.path} output/artist_release_relationship/part-* output/tracklist_tracks_relationship/part-* output/artist_tracks_relationship/part-* > output/relationships.tsv )
+  headers.unlink
+end
+
+output_artists_tsv
+output_tracklists_tsv
+output_tracks_tsv
+output_relationships_tsv
